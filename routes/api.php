e2fb62333b9499cce489df083940dfad87599e0e
Eldos Ulanbekov <ulanbekoveldos@gmail.com>
2024-02-26 23:21:30 +0600

    Настроил Swagger

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::prefix('v1')->group(function (){
+    Route::apiResource('categories',\App\Http\Controllers\Api\V1\CategoriesController::class);
+    Route::apiResource('products',\App\Http\Controllers\Api\V1\ProductController::class);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
